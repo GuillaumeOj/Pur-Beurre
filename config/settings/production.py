52c2134db76eeb5dc4c9322b7012dcff2f8638fb
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-17 20:15:07 +0200

    Set DEBUG to false for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import *
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".ojardias.io", "fast-shore-25529.herokuapp.com"]
 
