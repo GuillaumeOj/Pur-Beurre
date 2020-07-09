489423e2be98f7164c3e768dc1ffaf4345434ea2
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-09 16:33:51 +0200

    Remove the os.path.join use

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,8 +1,6 @@
 """
 Django settings for the local project.
 """
-import os
-
 from .base import *
 
 # Quick-start development settings - unsuitable for production
@@ -19,6 +17,6 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": str(BASE_DIR + "db.sqlite3"),
     }
 }
