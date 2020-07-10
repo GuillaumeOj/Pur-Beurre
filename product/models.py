b7ef229ff3e70a836a55dda239d943f632ef3d0b
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-10 13:32:53 +0200

    Add product manager and change the code field's type

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -35,12 +36,63 @@ class Brand(models.Model):
         return self.name
 
 
+class ProductManager(models.Manager):
+    def create_product(self, raw_product):
+        if self._valid_raw_product(raw_product):
+            product = self.create(
+                code=raw_product["code"],
+                name=raw_product["name"],
+                url=raw_product["url"],
+                nutriscore_grade=raw_product["nutriscore_grade"],
+                generic_name=raw_product["generic_name"],
+                quantity=raw_product["quantity"],
+                ingredients_text=raw_product["ingredients_text"],
+                image_url=raw_product["image_url"],
+                image_small_url=raw_product["image_small_url"],
+            )
+            return product
+
+    def _valid_raw_product(self, raw_product):
+        """
+        Validate the raw product's data
+        """
+        # Required fields
+        if not raw_product.get("code"):
+            raise ValidationError("Missing product's code")
+        if len(raw_product.get("code")) != 13:
+            raise ValidationError("Product's code is not at EAN-13 format")
+
+        if not raw_product.get("name"):
+            raise ValidationError("Missing product's name")
+        if len(raw_product.get("name")) > 100:
+            raise ValidationError("Product's name too long")
+
+        if not raw_product.get("nutriscore_grade"):
+            raise ValidationError("Missing product's nutriscore grade")
+        if len(raw_product.get("nutriscore_grade")) > 1:
+            raise ValidationError("Product's nutriscore grade too long")
+
+        if not raw_product.get("url"):
+            raise ValidationError("Missing product's url")
+        if len(raw_product.get("url")) > 250:
+            raise ValidationError("Product's url too long")
+
+        # Optionnal fields
+        if raw_product.get("generic_name") and len(raw_product.get("generic_name")) > 100:
+            raise ValidationError("Product's generic name too long")
+
+        if raw_product.get("quantity") and len(raw_product.get("quantity")) > 100:
+            raise ValidationError("Product's quantity too long")
+
+        return True
+
+
 class Product(models.Model):
     """
     Product from openfoodfact
     """
 
-    code = models.BigIntegerField(unique=True)
+    code = models.CharField(max_length=13, unique=True)
 
     name = models.CharField(max_length=100)
 
@@ -61,6 +113,8 @@ class Product(models.Model):
     categories = models.ManyToManyField(Category)
     stores = models.ManyToManyField(Store)
     brands = models.ManyToManyField(Brand)
+
+    objects = ProductManager()
 
     def __str__(self):
         return f"{self.code} - {self.name}"
