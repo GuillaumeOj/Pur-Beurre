b9effc5015ae4080d7dd2b7fd4ddad30375be98d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-17 20:26:33 +0200

    Add pagination to subsitutes

--- search/urls.py
+++ search/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "search"
 urlpatterns = [
     path("", views.find, name="find"),
+    path(
+        "<str:product_code>/<int:page>/", views.find_substitutes, name="find_substitutes"
+    ),
 ]
--- search/views.py
+++ search/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from product.models import Product
 from product.forms import ProductSearchForm
@@ -17,11 +18,35 @@ def find(request):
             product = Product.objects.find_product(form.cleaned_data["name"])
 
             if product:
-                substitutes = Product.objects.find_substitute(product.code)
-                context["product"] = product
-                context["substitutes"] = substitutes
+                return redirect(
+                    reverse("search:find_substitutes", args=[product.code, 1])
+                )
             else:
                 context["product"] = {"name": form.cleaned_data["name"]}
 
             return render(request, "search/substitutes.html", context=context)
     return redirect(reverse("homepage:index"))
+
+
+def find_substitutes(request, product_code, page):
+    form = ProductSearchForm()
+    context = {"product_search_form": form}
+
+    product = Product.objects.get(code=product_code)
+
+    if product:
+        context["product"] = product
+        substitutes = Product.objects.find_substitute(product.code)
+
+        if substitutes:
+            # Paginate the subsitutes result
+            pagination = Paginator(substitutes, 6, orphans=3)
+            try:
+                context["substitutes"] = pagination.page(page)
+            except PageNotAnInteger:
+                context["substitutes"] = pagination.page(1)
+            except EmptyPage:
+                context["substitutes"] = pagination.page(pagination.num_pages)
+    else:
+        context["product"] = {"name": form.cleaned_data["name"]}
+    return render(request, "search/substitutes.html", context=context)
