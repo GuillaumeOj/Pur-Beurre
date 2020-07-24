b11c21ef9530908ad5c7614828832355bb346cb6
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-24 18:10:50 +0200

    Use auto-completion result for searching substitutes

--- static/dist/js/completion.js
+++ static/dist/js/completion.js
@@ -4,10 +4,10 @@ class Search {
 		this.form = form;
 		this.input = this.form.querySelector("#id_name");
 		this.display_result = document.getElementById(display_result);
+		this.results = this.display_result.getElementsByTagName("li");
 
 		/* Define all events listener */
 		this.form.name.addEventListener("keyup", () => { this.process() });
-		this.form.name.addEventListener("focusout", () => { this.clear_result() });
 
 		/* Limit time response for fectch */
 		this.HttpHeaders = new Headers();
@@ -16,7 +16,7 @@ class Search {
 
 	process() {
 		/* Find and display products'names */
-		if (this.form.name.value.length < 2) {
+		if (this.form.name.value.length < 3) {
 			/* Avoid autocompletion for words under 2 letter */
 			this.clear_result();
 		} else {
@@ -45,6 +45,7 @@ class Search {
 						}
 					}
 				}
+				this.update_results();
 			})
 			.catch(error => console.log(error));
 		}
@@ -55,10 +56,29 @@ class Search {
 		this.display_result.innerHTML = "";
 		this.display_result.classList.add("hidden");
 	}
+
+	update_results() {
+		this.results = this.display_result.getElementsByTagName("li");
+		if (this.results.length > 0) {
+			for (let i in this.results) {
+				this.results.item(i).addEventListener("click", event => {
+					this.form.name.value = event.target.textContent;
+					this.clear_result();
+					this.form.submit();
+				});
+			}
+		}
+	}
 }
 
-let search_main_form = document.getElementById("search_main");
-new Search(search_main_form, "results_main");
+window.addEventListener("load", () => {
+	let search_main_form = document.getElementById("search_main");
+	if (search_main_form != null) {
+		new Search(search_main_form, "results_main");
+	}
 
-let search_short_form = document.getElementById("search_short");
-new Search(search_short_form, "results_short");
+	let search_short_form = document.getElementById("search_short");
+	if (search_short_form != null) {
+		new Search(search_short_form, "results_short");
+	}
+});
