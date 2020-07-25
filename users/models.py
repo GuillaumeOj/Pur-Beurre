135362b971a23cab34fbf976dacbe4aa5464d9aa
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-25 20:34:25 +0200

    Delete unused imports in users' models

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from product.models import Favorite, Product
+from product.models import Favorite
 
 # Create your models here.
 
