004591088300c8c4a36408ea1ed26cc0ccc605a7
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-26 12:28:26 +0200

    Change add_css for add_attrs, and allow passing ohter attributes than class

--- homepage/templatetags/filters.py
+++ homepage/templatetags/filters.py
@@ -1,8 +1,15 @@
 from django import template
+from django.http import QueryDict
 
 register = template.Library()
 
 
-@register.filter(name="add_css")
-def add_css(field, css):
-    return field.as_widget(attrs={"class": css})
+@register.filter(name="add_attrs")
+def add_attrs(field, attrs):
+    # Transform attributes in a Python dict
+    attrs = dict(QueryDict(query_string=attrs, mutable=True))
+
+    # Transform each dict item in a str instead of a list
+    for attr, value in attrs.items():
+        attrs[attr] = " ".join(value)
+    return field.as_widget(attrs=attrs)
