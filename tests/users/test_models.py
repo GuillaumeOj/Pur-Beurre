cdb73534a9b2e4ebefd0bd91a1028e4e13b5f89b
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-26 19:35:33 +0200

    Add test for the user model

---
+++ tests/users/test_models.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+
+from users.models import User
+
+
+class UserModelsTests(TestCase):
+    fixtures = ["favorite.json", "product.json", "user.json", "category.json"]
+
+    def get_user(self):
+        return User.objects.all().first()
+
+    def create_user(self, email, first_name, last_name, password):
+        return User.objects.create_user(
+            email=email, first_name=first_name, last_name=last_name, password=password
+        )
+
+    def create_superuser(self, email, first_name, last_name, password):
+        return User.objects.create_superuser(
+            email=email, first_name=first_name, last_name=last_name, password=password
+        )
+
+    def test_user_printing_name(self):
+        user = self.get_user()
+
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.__str__(), user.email)
+
+    def test_user_is_created_with_all_fields(self):
+        user = self.create_user(
+            "test@test.com", "testeur", "testeur", "i-am-a-strong-password-5"
+        )
+
+        self.assertIsInstance(user, User)
+
+    def test_user_is_created_with_all_required_fields_only(self):
+        user = self.create_user(
+            "test@test.com", "testeur", "", "i-am-a-strong-password-5"
+        )
+
+        self.assertIsInstance(user, User)
+
+    def test_user_is_not_created_with_email_missing(self):
+        with self.assertRaises(ValueError):
+            self.create_user("", "testeur", "testeur", "i-am-a-strong-password-5")
+
+    def test_user_is_not_created_with_first_name_missing(self):
+        with self.assertRaises(ValueError):
+            self.create_user("test@test,com", "", "testeur", "i-am-a-strong-password-5")
+
+    def test_user_is_not_created_with_password_missing(self):
+        with self.assertRaises(ValueError):
+            self.create_user("test@test,com", "testeur", "testeur", "")
+
+    def test_superuser_is_created(self):
+        superuser = self.create_superuser(
+            "test@test.com", "testeur", "testeur", "i-am-a-strong-password-5"
+        )
+
+        self.assertIsInstance(superuser, User)
