a7848ae039e6d309088a2e3655793d62f685a909
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-26 10:10:48 +0200

    Add tests for product's views

--- tests/product/test_views.py
+++ tests/product/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, override_settings
+from django.test import TestCase, override_settings, Client
 from django.shortcuts import reverse
 
 from product.models import Product
@@ -12,10 +12,14 @@ class ProductViewsTests(TestCase):
     fixtures = ["favorite.json", "product.json", "user.json", "category.json"]
 
     def setUp(self):
+        self.client = Client(HTTP_REFERER="http://www.qwant.fr")
         self.user = User.objects.all().first()
 
     def get_nutella(self):
         return Product.objects.filter(name__icontains="nut").first()
+
+    def get_pate(self):
+        return Product.objects.filter(name__icontains="Nocciolata").first()
 
     def test_sheet_is_loading(self):
         nutella = self.get_nutella()
@@ -24,6 +28,19 @@ class ProductViewsTests(TestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_save_favorite_is_redirecting(self):
+        self.client.force_login(self.user)
+        product = self.get_nutella()
+        substitute = self.get_pate()
+
+        url = reverse(
+            "product:save",
+            kwargs={"product_code": product.code, "substitute_code": substitute.code},
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 302)
 
     def test_favorites_is_loading(self):
         self.client.force_login(self.user)
