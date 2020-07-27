657feae4d68811042bfd33189bef7c6e7d395069
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-27 18:00:19 +0200

    Add tests for get_products and find_substitutes

--- tests/product/test_product_manager.py
+++ tests/product/test_product_manager.py
@@ -32,7 +32,7 @@ class ProductManagerTests(CustomTestCase):
         self.assertIsInstance(product, Product)
         self.assertEqual(product, self.nut)
 
-    def test_product_by_name_with_wrong_name(self):
+    def test_get_product_by_name_with_wrong_name(self):
         product = Product.objects.get_product_by_name("qwerty")
 
         self.assertNotIsInstance(product, Product)
@@ -51,6 +51,11 @@ class ProductManagerTests(CustomTestCase):
 
         self.assertFalse(products)
 
+    def test_get_products_by_name_with_no_name(self):
+        products = Product.objects.get_products_by_name("")
+
+        self.assertFalse(products)
+
     def test_find_substitutes(self):
         substitutes = Product.objects.find_substitutes(self.nutella.code)
 
@@ -60,3 +65,13 @@ class ProductManagerTests(CustomTestCase):
         for substitute in substitutes:
             self.assertLess(substitute.nutriscore_grade, self.nutella.nutriscore_grade)
             self.assertNotEqual(substitute, self.nutella)
+
+    def test_find_substitutes_with_wrong_code(self):
+        substitutes = Product.objects.find_substitutes("qwerty")
+
+        self.assertFalse(substitutes)
+
+    def test_find_substitutes_with_no_code(self):
+        substitutes = Product.objects.find_substitutes("")
+
+        self.assertFalse(substitutes)
