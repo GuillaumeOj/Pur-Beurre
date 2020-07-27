f9e4f616d7ba1c0b6630bda9b0e32e3ed752d852
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-27 16:35:24 +0200

    Mock Product for testing the search app's views

--- tests/search/test_views.py
+++ tests/search/test_views.py
@@ -1,4 +1,5 @@
 from json import loads
+from unittest.mock import patch
 
 from django.shortcuts import reverse
 from django.test import override_settings
@@ -7,36 +8,67 @@ from product.models import Product
 from tests.custom import CustomTestCase
 
 
+class MockProductManager:
+    def __init__(self, products):
+        self.products = products
+
+    def get_product_by_code(self, *args, **kwargs):
+        return self.products
+
+    def get_product_by_name(self, *args, **kwargs):
+        return self.products
+
+    def get_products_by_name(self, *args, **kwargs):
+        return self.products
+
+
+class MockProduct:
+    def __init__(self, products):
+        self.products = products
+        self.objects = MockProductManager(products)
+
+
 @override_settings(
     STATICFILES_STORAGE="django.contrib.staticfiles.storage.StaticFilesStorage"
 )
 class SearchViewsAutoCompletionTests(CustomTestCase):
+    def setUp(self):
+        self.products = Product.objects.filter(name__icontains="nut")[:2]
+
     def test_auto_completion_return_json(self):
         url = reverse("search:auto_completion")
-        response = self.client.post(url, data={"name": "nut"})
+        mock_product = MockProduct(self.products).objects.get_products_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.post(url, data={"name": "nut"})
 
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(loads(response.content).get("products_names"))
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(loads(response.content).get("products_names"))
 
     def test_auto_completion_return_empty_list_if_there_is_no_matching_product(self):
         url = reverse("search:auto_completion")
-        response = self.client.post(url, data={"name": "qwerty"})
+        mock_product = MockProduct(self.products).objects.get_products_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.post(url, data={"name": "qwerty"})
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("products_names", loads(response.content))
-        self.assertEqual(loads(response.content).get("products_names"), [])
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("products_names", loads(response.content))
+            self.assertEqual(loads(response.content).get("products_names"), [])
 
     def test_auto_completion_return_no_json_if_the_form_is_invalid(self):
         url = reverse("search:auto_completion")
-        response = self.client.post(url, data={"name": ""})
+        mock_product = MockProduct(self.products).objects.get_products_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.post(url, data={"name": ""})
 
-        self.assertEqual(response.status_code, 400)
+            self.assertEqual(response.status_code, 400)
 
     def test_auto_completion_redirect_if_accessing_with_get(self):
         url = reverse("search:auto_completion")
-        response = self.client.get(url)
+        mock_product = MockProduct(self.products).objects.get_products_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.status_code, 302)
 
 
 @override_settings(
@@ -48,15 +80,19 @@ class SearchViewsGetProductTests(CustomTestCase):
 
     def test_get_product_redirect_to_get_substitutes_if_found_a_product(self):
         url = reverse("search:get_product")
-        response = self.client.post(url, data={"name": "Nutella"})
+        mock_product = MockProduct(self.nutella).objects.get_product_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.post(url, data={"name": "Nutella"})
 
-        self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.status_code, 302)
 
     def test_get_product_render_page_if_no_product_was_found(self):
         url = reverse("search:get_product")
-        response = self.client.post(url, data={"name": "qwerty"})
+        mock_product = MockProduct(self.nutella).objects.get_product_by_name
+        with patch("product.models.Product", mock_product):
+            response = self.client.post(url, data={"name": "qwerty"})
 
-        self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 200)
 
     def test_get_product_redirect_if_accessing_with_get(self):
         url = reverse("search:get_product")
