7aab23fcb39a62ed31efee25220fadd3f5f847c5
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-07-28 09:40:53 +0200

    Add tests for Category

--- tests/product/test_category_models.py
+++ tests/product/test_category_models.py
@@ -1,3 +1,5 @@
+from django.test import ValidationError
+
 from product.models import Category
 from tests.custom import CustomTestCase
 
@@ -11,3 +13,23 @@ class CategoryModelsTests(CustomTestCase):
 
         self.assertIsInstance(category, Category)
         self.assertEqual(category.__str__(), category.name)
+
+    def test_create_category(self):
+        category = Category.objects.create(name="Test")
+
+        self.assertIsInstance(category, Category)
+        self.assertEqual(category.name, "Test")
+        with not self.assertRaises(ValidationError):
+            category.full_clean()
+
+    def test_create_category_with_name_too_short(self):
+        category = Category.objects.create(name="T")
+
+        with self.assertRaises(ValidationError):
+            category.full_clean()
+
+    def test_create_category_with_name_too_long(self):
+        category = Category.objects.create(name=101 * "T")
+
+        with self.assertRaises(ValidationError):
+            category.full_clean()
