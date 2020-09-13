050e2b390e068272058388359763592f8ed53f54
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-13 17:32:59 +0200

    Add config settings for travis only

---
+++ config/settings/travis.py
@@ -0,0 +1,21 @@
+# Import the base settings
+from .base import *
+
+
+ALLOWED_HOSTS = ["*"]
+
+
+
+
+SECRET_KEY = "foo_key_for_travis"
+DEBUG = False
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": '',
+        "USER": 'postgres',
+        "PASSWORD": '',
+        "HOST": '',
+        "PORT": '',
+    }
+}
