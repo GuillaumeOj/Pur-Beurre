3f779887141d906a52bd7856cd30eb3484d99659
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-13 17:54:02 +0200

    Black travis configuration file

--- config/settings/travis.py
+++ config/settings/travis.py
@@ -5,17 +5,15 @@ from .base import *
 ALLOWED_HOSTS = ["*"]
 
 
-
-
 SECRET_KEY = "foo_key_for_travis"
 DEBUG = False
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": '',
-        "USER": 'postgres',
-        "PASSWORD": '',
-        "HOST": '',
-        "PORT": '',
+        "NAME": "",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
