5420d75164572ddc9275c8cfd178279abb01f256
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-25 00:24:33 +0200

    Remove heroku settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,6 @@
 """Django settings for the local project."""
 import os
 
-import dj_database_url
 from dotenv import find_dotenv
 from dotenv import load_dotenv
 
@@ -11,31 +10,16 @@ from .base import *
 
 ALLOWED_HOSTS = ["projet-8.ojardias.io", "pur-beurre.ojardias.io"]
 
-# Heroku database
-if os.getenv("ENV_HOST") == "HEROKU":
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = True if os.getenv("DEBUG") == "True" else False
-    MIDDLEWARE.append(
-        "whitenoise.middleware.WhiteNoiseMiddleware",
-    )
-
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASES = {}
-    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-else:
-    load_dotenv(find_dotenv())
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = True if os.getenv("DEBUG") == "True" else False
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("DATABASE_NAME"),
-            "USER": os.getenv("DATABASE_USER"),
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-            "HOST": os.getenv("DATABASE_HOST"),
-            "PORT": os.getenv("DATABASE_PORT"),
-        }
+load_dotenv(find_dotenv())
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = True if os.getenv("DEBUG") == "True" else False
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
+}
