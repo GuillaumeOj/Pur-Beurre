960f6c5b0aebe9d7253b2b4db4802d8d39c1220c
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-26 08:46:30 +0200

    Trigger an error for testing sentry

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,4 +7,5 @@ app_name = "homepage"
 urlpatterns = [
     path("", views.index, name="index"),
     path("disclaimer/", views.disclaimer, name="disclaimer"),
+    path("sentry-debug/", views.trigger_error),
 ]
